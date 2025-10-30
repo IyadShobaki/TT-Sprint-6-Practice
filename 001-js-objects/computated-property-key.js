160f1ffb07238b7d604c502f0ed5e2ea03f07907
Iyad <iyadshobaki82@gmail.com>
2025-10-30 16:32:33 -0400

    first commit

---
+++ 001-js-objects/computated-property-key.js
@@ -0,0 +1,28 @@
+const phonebook = {
+  Lily: {
+    mobile: "+16235554420",
+    work: "+12445552374",
+    home: "+19775552223",
+  },
+  Tim: {
+    mobile: "+4235555616",
+  },
+};
+
+function addPhoneNumber(name, numberType, number) {
+  if (name in phonebook == false) {
+    phonebook[name] = {};
+    phonebook[name][numberType] = number;
+  } else {
+    phonebook[name][numberType] = number;
+  }
+}
+
+// Adding a new phone number for an existing contact should still work.
+addPhoneNumber("Tim", "home", "+4235555212");
+console.log(phonebook.Tim.home); // "+4235555212"
+console.log(phonebook.Tim.mobile); // "+4235555616"
+
+// Adding a new phone number for an new contact should also work.
+addPhoneNumber("Kyle", "mobile", "+5553142534");
+console.log(phonebook.Kyle.mobile); // "+5553142534"
