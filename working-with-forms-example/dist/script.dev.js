baea2a81241f3916b6942f5fa415ca3a520f0afd
Iyad <iyadshobaki82@gmail.com>
2025-11-04 13:51:07 -0500

    Add form validation
    - validate input fields are not empty and check inside form input listener
    
    - add function to disable and enable add button

--- working-with-forms-example/dist/script.dev.js
+++ working-with-forms-example/dist/script.dev.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var songsContainer = document.querySelector(".songs-container");
+var addButton = document.querySelector(".input__btn_action_add");
 var form = document.forms.add;
 var artist = form.elements.artist;
 var title = form.elements.title;
@@ -13,9 +14,24 @@ function addSong(artistValue, titleValue) {
   songsContainer.append(songElement);
 }
 
+function setSubmitButtonState(isFormValid) {
+  if (isFormValid) {
+    addButton.removeAttribute("disabled");
+    addButton.classList.remove("input__btn_disabled");
+  } else {
+    addButton.setAttribute("disabled", true);
+    addButton.classList.add("input__btn_disabled");
+  }
+}
+
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
   addSong(artist.value, title.value);
   artist.value = "";
   title.value = "";
+  setSubmitButtonState(false);
+});
+form.addEventListener("input", function (evt) {
+  var isValid = artist.value.length > 0 && title.value.length > 0;
+  setSubmitButtonState(isValid);
 });
--- working-with-forms-example/script.js
+++ working-with-forms-example/script.js
@@ -1,7 +1,9 @@
 const songsContainer = document.querySelector(".songs-container");
+const addButton = document.querySelector(".input__btn_action_add");
 const form = document.forms.add;
 const artist = form.elements.artist;
 const title = form.elements.title;
+
 function addSong(artistValue, titleValue) {
   const songTemplate = document.querySelector("#song-template").content;
   const songElement = songTemplate.cloneNode(true);
@@ -12,9 +14,25 @@ function addSong(artistValue, titleValue) {
   songsContainer.append(songElement);
 }
 
+function setSubmitButtonState(isFormValid) {
+  if (isFormValid) {
+    addButton.removeAttribute("disabled");
+    addButton.classList.remove("input__btn_disabled");
+  } else {
+    addButton.setAttribute("disabled", true);
+    addButton.classList.add("input__btn_disabled");
+  }
+}
+
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
   addSong(artist.value, title.value);
   artist.value = "";
   title.value = "";
+  setSubmitButtonState(false);
+});
+
+form.addEventListener("input", function (evt) {
+  const isValid = artist.value.length > 0 && title.value.length > 0;
+  setSubmitButtonState(isValid);
 });
