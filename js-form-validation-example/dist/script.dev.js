da5d0af17447bbe699b782d5c31660ad71caadb9
Iyad <iyadshobaki82@gmail.com>
2025-11-05 10:33:47 -0500

    Add checkInputValidity function to check form input

--- js-form-validation-example/dist/script.dev.js
+++ js-form-validation-example/dist/script.dev.js
@@ -3,12 +3,27 @@
 var formElement = document.querySelector(".form");
 var formInput = formElement.querySelector(".form__input");
 
-var showError = function showError(input) {// add the error class to the input element
+var showError = function showError(input) {
+  // add the error class to the input element
+  input.classList.add("form__input_type_error");
 };
 
-var hideError = function hideError(input) {// remove the error class from the input element
+var hideError = function hideError(input) {
+  // remove the error class from the input element
+  input.classList.remove("form__input_type_error");
+};
+
+var checkInputValidity = function checkInputValidity() {
+  if (!formInput.validity.valid) {
+    showError(formInput);
+  } else {
+    hideError(formInput);
+  }
 };
 
 formElement.addEventListener("submit", function (evt) {
   evt.preventDefault();
+});
+formElement.addEventListener("input", function (evt) {
+  checkInputValidity();
 });
--- js-form-validation-example/script.js
+++ js-form-validation-example/script.js
@@ -3,12 +3,26 @@ const formInput = formElement.querySelector(".form__input");
 
 const showError = (input) => {
   // add the error class to the input element
+  input.classList.add("form__input_type_error");
 };
 
 const hideError = (input) => {
   // remove the error class from the input element
+  input.classList.remove("form__input_type_error");
+};
+
+const checkInputValidity = () => {
+  if (!formInput.validity.valid) {
+    showError(formInput);
+  } else {
+    hideError(formInput);
+  }
 };
 
 formElement.addEventListener("submit", function (evt) {
   evt.preventDefault();
+});
+
+formElement.addEventListener("input", function (evt) {
+  checkInputValidity();
 });
