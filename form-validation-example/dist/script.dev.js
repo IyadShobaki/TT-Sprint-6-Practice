870e7c73cb867b8ad1bdcac6d11503bae6485cb9
Iyad <iyadshobaki82@gmail.com>
2025-11-05 10:21:07 -0500

    Add another form and use JS to validate (show validity object)

--- form-validation-example/dist/script.dev.js
+++ form-validation-example/dist/script.dev.js
@@ -1,6 +1,14 @@
 "use strict";
 
 var formElement = document.querySelector(".form");
+var urlForm = document.forms.urlForm;
+var url = urlForm.elements.url;
 formElement.addEventListener("submit", function (evt) {
   evt.preventDefault();
+});
+urlForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+urlForm.addEventListener("input", function (evt) {
+  console.log(evt.target.validity);
 });
--- form-validation-example/script.js
+++ form-validation-example/script.js
@@ -1,5 +1,15 @@
 const formElement = document.querySelector(".form");
+const urlForm = document.forms.urlForm;
+const url = urlForm.elements.url;
 
 formElement.addEventListener("submit", function (evt) {
   evt.preventDefault();
+});
+
+urlForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+
+urlForm.addEventListener("input", function (evt) {
+  console.log(evt.target.validity);
 });
