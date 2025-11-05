782b5fadd01b33ec3e9bfb463be0f8a46c313dc2
Iyad <iyadshobaki82@gmail.com>
2025-11-05 10:58:52 -0500

    Add error message span to show validation message

--- js-form-validation-example/dist/script.dev.js
+++ js-form-validation-example/dist/script.dev.js
@@ -1,11 +1,17 @@
 "use strict";
 
 var formElement = document.querySelector(".form");
-var formInput = formElement.querySelector(".form__input");
+var formInput = formElement.querySelector(".form__input"); // 1. Declare the formError constant.
 
-var showError = function showError(input) {
+var formError = formElement.querySelector(".".concat(formInput.id, "-error"));
+
+var showError = function showError(input, errorMessage) {
   // add the error class to the input element
-  input.classList.add("form__input_type_error");
+  input.classList.add("form__input_type_error"); // 2. Assign errorMessage to the textContent for formError.
+
+  formError.textContent = errorMessage; // 3. Add the form__input-error_active class to formError.
+
+  formError.classList.add("form__input-error_active");
 };
 
 var hideError = function hideError(input) {
@@ -15,7 +21,9 @@ var hideError = function hideError(input) {
 
 var checkInputValidity = function checkInputValidity() {
   if (!formInput.validity.valid) {
-    showError(formInput);
+    showError(formInput, formInput.validationMessage
+    /* 4. Make the error message the function's second parameter. */
+    );
   } else {
     hideError(formInput);
   }
--- js-form-validation-example/script.js
+++ js-form-validation-example/script.js
@@ -1,9 +1,15 @@
 const formElement = document.querySelector(".form");
 const formInput = formElement.querySelector(".form__input");
+// 1. Declare the formError constant.
+const formError = formElement.querySelector(`.${formInput.id}-error`);
 
-const showError = (input) => {
+const showError = (input, errorMessage) => {
   // add the error class to the input element
   input.classList.add("form__input_type_error");
+  // 2. Assign errorMessage to the textContent for formError.
+  formError.textContent = errorMessage;
+  // 3. Add the form__input-error_active class to formError.
+  formError.classList.add("form__input-error_active");
 };
 
 const hideError = (input) => {
@@ -13,7 +19,10 @@ const hideError = (input) => {
 
 const checkInputValidity = () => {
   if (!formInput.validity.valid) {
-    showError(formInput);
+    showError(
+      formInput,
+      formInput.validationMessage /* 4. Make the error message the function's second parameter. */
+    );
   } else {
     hideError(formInput);
   }
